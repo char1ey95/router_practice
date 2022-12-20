d3d637d1cf0d7360f8c3537e90f9d678596aa97d
char1ey <ckstn410@gmail.com>
2022-12-20 09:19:49 +0900

    feat: server.js router 설정

--- server.js
+++ server.js
@@ -10,13 +10,32 @@ nunjucks.configure('views', {
     watch: true,
 })
 
+// 미들웨어 영역 시작 ====================================================
+
+
 app.get('/', (req, res) => {
     res.render('index.html')
 })
 
 app.get('/list', (req, res) => {
-
+    // 내부적으로 path.join을 해주기 때문에 이렇게 적는다.
+    res.render('board/list.html')
 })
+
+app.get('/view', (req, res) => {
+    res.render('board/view.html')
+})
+
+app.get('/write', (req, res) => {
+    res.render('board/write.html')
+})
+
+app.get('/modify', (req, res) => {
+    res.render('board/modify.html')
+})
+
+
+// 미들웨어 영역 끝 =====================================================
 
 app.listen(PORT, () => {
     console.log('server start')
