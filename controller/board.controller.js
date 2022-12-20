74ef0fc9bc40c7aa30ec45aa01a0908479302262
Hanjin <csa0545@gamil.com>
2022-12-20 17:06:17 +0900

    수정 완료

--- controller/board.controller.js
+++ controller/board.controller.js
@@ -22,14 +22,14 @@ exports.Pwrite = (req, res) => {
     const { content, subject, name } = req.body
     items.push({ content, subject, name })
 
-    res.redirect(`/view?index=${items.length - 1}`)
+    res.redirect(`/board/view?index=${items.length - 1}`)
 }
 
 exports.view = (req, res) => {
     const { index } = req.query
     const item = {
         ...items[index],
-        index
+        index,
     }
     res.render('board/view.html', { item })
 }
@@ -49,11 +49,11 @@ exports.Pmodify = (req, res) => {
     items[index].content = content
     items[index].name = name
 
-    res.redirect(`/view?index=${index}`)
+    res.redirect(`/board/view?index=${index}`)
 }
 
 exports.delete = (req, res) => {
     const { index } = req.query
     items.splice(index, 1)
-    res.redirect('/list')
+    res.redirect('/board/list')
 }
